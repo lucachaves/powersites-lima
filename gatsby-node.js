c82c0a11bb8d8c758493e594ed0c419ef40039a1
Luiz Chaves <lucachaves@gmail.com>
2020-05-03 13:55:56 -0300

    initial structure

---
+++ gatsby-node.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const contadorTemplate = path.resolve('src', 'templates', 'contador.jsx');
+  const pages = [1, 2, 3, 4];
+
+  pages.forEach((page) => {
+    createPage({
+      path: `contador-${page}`,
+      component: contadorTemplate,
+      context: {
+        page: page,
+      },
+    });
+  });
+
+}
---
+++ src/components/BtnWhatsapp.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import wa from '../assets/btn-whatsapp.png';
+
+const BtnWhatsapp = () => {
+  return (
+    <div className="bg-white shadow rounded px-5 py-2 flex flex-col sm:flex-row items-center mt-6 sm:mt-0">
+      <img src={wa} alt="whatsapp"/>
+      <div className="ml-2">
+        <h3 className="font-bold text-2xl">Orçar seu Projeto!</h3>
+        <p className="text-xs">Projeto e orçamento sem compromisso.</p>
+        <p className="font-bold">(35) 9999-9999</p>
+      </div>
+    </div>
+  );
+};
+
+export default BtnWhatsapp;
